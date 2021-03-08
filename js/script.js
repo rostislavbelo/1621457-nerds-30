b5fc05d3d7ba11d1fa92eccb20e3346b36bb5f88
Рост <rabelousov@gmail.com>
2021-03-08 11:34:14 +0300

    сделана доступная разметка главной страницы

--- js/script.js
+++ js/script.js
@@ -4,10 +4,19 @@ const modal_feedback = document.querySelector(".modal--feedback");
 const slider_bullit = document.querySelectorAll(".slider-pager-link");
 const sort_button = document.querySelectorAll(".sort-type-main-link");
 const sort_direction = document.querySelectorAll(".sort-type-direction-link");
+const form = document.querySelector(".appointment-form");
+const name = document.querySelector(".appointment-name");
+const mail = document.querySelector(".appointment-email");
+
 
 address_button.addEventListener("click", function (e) {
   e.preventDefault();
   modal_feedback.classList.add("modal--show");
+  modal_feedback.classList.remove("popup-error");
+  name.value = "";
+  name.classList.remove("error");
+  mail.value = "";
+  mail.classList.remove("error");
 });
 
 modal_closure.addEventListener("click", function (e) {
@@ -49,6 +58,29 @@ sort_direction.forEach(function (el, i) {
     this.classList.add("sort-type-direction-link--active");
 
   });
+});
+
+name.required = false;
+mail.required = false;
+
+form.addEventListener("submit", function(e){
+  if (!name.value || !mail.value || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail.value)) {
+    e.preventDefault();
+    modal_feedback.classList.remove('popup-error');
+    modal_feedback.offsetWidth = modal_feedback.offsetWidth;
+    modal_feedback.classList.add('popup-error');
+
+    if (!name.value) {
+      name.classList.add("error");
+
+    }
+    console.log(mail.value)
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail.value)){
+      mail.classList.add("error");
+    }
+
+  }
+
 });
 
 
